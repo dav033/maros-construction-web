1379ed2f1993880eaf76f148ec546fa1a3649bda
dav033 <87620700+dav033@users.noreply.github.com>
2024-04-24 12:18:57 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: "https://www.my-site.dev",
+  build: {
+    inlineStylesheets: "always",
+  },
+  compressHTML: true,
+  prefetch: true,
+  devToolbar: {
+    enabled: false,
+  },
 });
