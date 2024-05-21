f24dc120d772fe22dd30698f5fbc5d53bcdce485
david theran morales <david.theran030201@gmail.com>
2024-05-21 14:23:36 -0500

    add accesibility improves

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
-import { imagetools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), imagetools()],
+  integrations: [react()],
   output: "server",
   adapter: netlify(),
   adapter: netlify({
     edgeMiddleware: true,
   }),
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  },
   vite: {
     ssr: {
       noExternal: ["path-to-regexp"],
