646ca98db8c3c037c1ae6b42516828a9202b995c
david theran morales <david.theran030201@gmail.com>
2024-05-21 14:27:34 -0500

    add accesibility improves

--- src/static.js
+++ src/static.js
@@ -1,82 +1,112 @@
 import images from "./images";
 
-
 const concreteData = [
-    {
-        title: "Concrete Restoration & Waterproofing",
-        image: images.waterproofing.src,
-        description:
-            "We specialize in restoring and waterproofing concrete structures. Whether it’s repairing cracks, reinforcing foundations, or applying protective coatings, we ensure longevity and resilience.",
-    },
+  {
+    title: "Concrete Restoration & Waterproofing",
+    image: images.waterproofing.src,
+    description:
+      "We specialize in restoring and waterproofing concrete structures. Whether it’s repairing cracks, reinforcing foundations, or applying protective coatings, we ensure longevity and resilience.",
+  },
 
-    {
-        title: "Concrete Works",
-        image: images.concrete.src,
-        description:
-            "Our expertise extends to all aspects of concrete construction. From pouring foundations to creating custom designs, we bring precision and creativity to every project.",
-    },
+  {
+    title: "Concrete Works",
+    image: images.concrete.src,
+    description:
+      "Our expertise extends to all aspects of concrete construction. From pouring foundations to creating custom designs, we bring precision and creativity to every project.",
+  },
 
-    {
-        title: "Driveways & Floors",
-        image: images.driveway.src,
-        description:
-            "Need a stunning driveway or durable flooring? Look no further. We craft driveways that enhance curb appeal and floors that withstand daily wear and tear.",
-    },
+  {
+    title: "Driveways & Floors",
+    image: images.driveway.src,
+    description:
+      "Need a stunning driveway or durable flooring? Look no further. We craft driveways that enhance curb appeal and floors that withstand daily wear and tear.",
+  },
 
-    {
-        title: "Home & Apartment Renovations",
-        image: images.home.src,
-        description:
-            "Transforming living spaces is our passion. Whether it’s a cozy apartment or a spacious home, we handle renovations with care, from layout adjustments to aesthetic upgrades.",
-    },
+  {
+    title: "Home & Apartment Renovations",
+    image: images.home.src,
+    description:
+      "Transforming living spaces is our passion. Whether it’s a cozy apartment or a spacious home, we handle renovations with care, from layout adjustments to aesthetic upgrades.",
+  },
 
-    {
-        title: "Home Additions",
-        image: images.additions.src,
-        description:
-            "Expanding your property? We’re here to help. Our team designs and constructs thoughtful additions, seamlessly integrating them with your existing structure.",
-    },
+  {
+    title: "Home Additions",
+    image: images.additions.src,
+    description:
+      "Expanding your property? We’re here to help. Our team designs and constructs thoughtful additions, seamlessly integrating them with your existing structure.",
+  },
 ];
 
 const generalQuestions = [
-    {
-        question: "What services do you provide?",
-        answer: "We provide a full range of construction services, we specializes in concrete restoration, home additions, and apartment renovations."
-    },
-    {
-        question: "What areas do you serve?",
-        answer: "Maros Construction proudly serves the vibrant communities of South Florida, including Miami-Dade County, Broward County, and West Palm Beach"
-    },
-    {
-        question: "What types of materials do you use?",
-        answer: "We use high-quality materials for all of our projects, including wood, metal, and stone."
-    },
-    {
-        question: "Do you offer financing options?",
-        answer: "Yes, we offer several financing options for our clients."
-    },
-    {
-        question: "Do you provide warranties?",
-        answer: "Yes, we provide a limited warranty on all of our projects."
-    },
-    {
-        question: "Do you offer design services?",
-        answer: "Yes, we have a team of experienced designers who can help you create the perfect space."
-    },
-    {
-        question: "What is your timeline for completing projects?",
-        answer: "We strive to complete all projects within the agreed-upon timeline."
-    },
-    {
-        question: "Are your contractors insured?",
-        answer: "Yes, all of our contractors are fully insured and bonded.  "
-    }
-]
+  {
+    question: "What services do you provide?",
+    answer:
+      "We provide a full range of construction services, we specializes in concrete restoration, home additions, and apartment renovations.",
+  },
+  {
+    question: "What areas do you serve?",
+    answer:
+      "Maros Construction proudly serves the vibrant communities of South Florida, including Miami-Dade County, Broward County, and West Palm Beach",
+  },
+  {
+    question: "What types of materials do you use?",
+    answer:
+      "We use high-quality materials for all of our projects, including wood, metal, and stone.",
+  },
+  {
+    question: "Do you offer financing options?",
+    answer: "Yes, we offer several financing options for our clients.",
+  },
+  {
+    question: "Do you provide warranties?",
+    answer: "Yes, we provide a limited warranty on all of our projects.",
+  },
+  {
+    question: "Do you offer design services?",
+    answer:
+      "Yes, we have a team of experienced designers who can help you create the perfect space.",
+  },
+  {
+    question: "What is your timeline for completing projects?",
+    answer:
+      "We strive to complete all projects within the agreed-upon timeline.",
+  },
+  {
+    question: "Are your contractors insured?",
+    answer: "Yes, all of our contractors are fully insured and bonded.  ",
+  },
+];
+
+const opinions = [
+  {
+    rating: 5,
+    image:
+      "https://lh3.googleusercontent.com/a-/ALV-UjVWsA9x0kVsG6wTdod5CY93YxN9Q4XNrF5wPqz-mMfIYNev-Svh=w36-h36-p-rp-mo-br100",
+    username: "Javier Guzman",
+    date: "6/05/2024",
+    text: "Maros Construction exceeded my expectations with their outstanding concrete work. Their attention to detail and precision craftsmanship transformed my project into a masterpiece. I highly recommend Maros Construction for anyone seeking top-notch concrete services.",
+  },
+  {
+    rating: 5,
+    image:
+      "https://lh3.googleusercontent.com/a-/ALV-UjUcAuKTJ28h1d5g7VEoV5IPbnujY-LZdqxbuMjhXYDFJyyPayc=w36-h36-p-rp-mo-br100",
+    username: "Ritchard Nuccio",
+    date: "6/05/2024",
+    text: "I recently had the pleasure of working with Maros Construction on a home renovation project, and I couldn't be more impressed with the results. From start to finish, their team demonstrated an unparalleled level of craftsmanship and professionalism that truly sets them apart in the industry.",
+  },
+  {
+    rating: 5,
+    image:
+      "https://lh3.googleusercontent.com/a/ACg8ocIhyj9pfOY_t23snqeN9Vgy0tbN3OUnQ8qWx02WJTpyvD7gQg=w36-h36-p-rp-mo-br100",
+    username: "Sata Gusty",
+    date: "6/05/2024",
+    text: "",
+  },
+];
 
 const staticData = {
-    concreteData,
-    generalQuestions
+  concreteData,
+  generalQuestions,
 };
-
 
 export default staticData;
