21cc8dae06c6808bdfdee21134094aad941a3e0a
david theran morales <david.theran030201@gmail.com>
2024-05-20 22:45:19 -0500

    optimize images

--- src/images.js
+++ src/images.js
@@ -1,11 +1,11 @@
 
-import concrete from "./assets/images/1000246271_c561b.png"
-import driveway from "./assets/images/1000229765_1740c.png";
-import waterproofing from "./assets/images/1000249305_0194e.png"
-import home from "./assets/images/1000229731_e0979.png"
+import concrete from "./assets/images/1000246271_c561b.webp"
+import driveway from "./assets/images/1000229765_1740c.webp";
+import waterproofing from "./assets/images/1000249305_0194e.webp"
+import home from "./assets/images/1000229731_e0979.webp"
 import additions from "./assets/images/1200_800.webp"
-import about1 from "./assets/images/1000247660_69036.png"
-import about2 from "./assets/images/1000243960_d832b.png"
+import about1 from "./assets/images/1000247660_69036.webp"
+import about2 from "./assets/images/1000243960_d832b.webp"
 
 
 const images = {
