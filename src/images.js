2d85625490f3324b4ee78157f81ea5d940024892
david theran morales <david.theran030201@gmail.com>
2024-05-24 14:01:29 -0500

    move the images to public folder

--- src/images.js
+++ src/images.js
@@ -1,12 +1,22 @@
-
-import concrete from "./assets/images/1000246271_c561b.webp"
-import driveway from "./assets/images/1000229765_1740c.webp";
-import waterproofing from "./assets/images/1000249305_0194e.webp"
-import home from "./assets/images/1000229731_e0979.webp"
-import additions from "./assets/images/1200_800.webp"
-import about1 from "./assets/images/1000247660_69036.webp"
-import about2 from "./assets/images/1000243960_d832b.webp"
-
+import concrete from "../public/images/1000246271_c561b.webp";
+import driveway from "../public/images/1000229765_1740c.webp";
+import waterproofing from "../public/images/1000249305_0194e.webp";
+import home from "../public/images/1000229731_e0979.webp";
+import additions from "../public/images/1200_800.webp";
+import about1 from "../public/images/1000247660_69036.webp";
+import about2 from "../public/images/1000243960_d832b.webp";
+import imageAboutUsPrincipal from "../public/images/aboutUsPrincipal.webp";
+import imageMediumAboutUsPrincipal from "../public/images/aboutUsPrincipal-medium.webp";
+import imageSmallAboutUsPrincipal from "../public/images/aboutUsPrincipal-small.webp";
+import imagePresentation1 from "../public/images/imagePresentation1.webp";
+import imagePresentation2 from "../public/images/imagePresentation2.webp";
+import imagePresentation1Medium from "../public/images/imagePresentation1-medium.webp";
+import imagePresentation2Medium from "../public/images/imagePresentation2-medium.webp";
+import imagePresentation1Small from "../public/images/imagePresentation1-small.webp";
+import imagePresentation2Small from "../public/images/imagePresentation2-small.webp";
+import imageAboutPresentation from "../public/images/aboutImage.webp"
+import design from "../public/images/design.webp"
+import laws from "../public/images/laws.webp"
 
 const images = {
   concrete,
@@ -15,7 +25,19 @@ const images = {
   home,
   additions,
   about1,
-  about2
+  about2,
+  imageAboutUsPrincipal,
+  imageMediumAboutUsPrincipal,
+  imageSmallAboutUsPrincipal,
+  imagePresentation1,
+  imagePresentation2,
+  imagePresentation1Medium,
+  imagePresentation2Medium,
+  imagePresentation1Small,
+  imagePresentation2Small,
+  imageAboutPresentation,
+  design,
+  laws
 };
 
 export default images;
