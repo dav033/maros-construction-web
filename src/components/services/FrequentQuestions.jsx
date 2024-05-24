2f832eeff0d9ed65dc5b7bb86ed62eb3bb230b9f
david theran morales <david.theran030201@gmail.com>
2024-05-24 13:26:15 -0500

    optimizate components

--- src/components/services/FrequentQuestions.jsx
+++ src/components/services/FrequentQuestions.jsx
@@ -1,35 +1,31 @@
-import { useEffect, useState } from "react"
-import QuestionItem from "./QuestionItem.jsx"
-import "../../styles/FrequentQuestions.css"
+import { useState, useCallback } from "react";
+import QuestionItem from "./QuestionItem.jsx";
+import "../../styles/FrequentQuestions.css";
 import staticData from "../../static.js";
 
 export default function FrequentQuestions() {
-    const [openQuestionId, setOpenQuestionId] = useState(null);
+  const [openQuestionId, setOpenQuestionId] = useState(null);
 
-    const [general, setGeneral] = useState(staticData.generalQuestions.map((item, index) => ({ id: index + 1, ...item })));
+  const generalQuestions = staticData.generalQuestions.map((item, index) => ({
+    id: index + 1,
+    ...item,
+  }));
 
+  const handleQuestionClick = useCallback((id) => {
+    setOpenQuestionId((prevId) => (prevId === id ? null : id));
+  }, []);
 
-    const handleQuestionClick = (id) => {
-        setOpenQuestionId(openQuestionId === id ? null : id);
-    }
-
-    return (
-        <div className="frequentQuestions">
-
-            <h1 className="title">Frequently asked questions</h1>
-            {
-                general.map((question, index) => (
-                    <QuestionItem
-                        key={question.id}
-                        question={question}
-                        isOpen={question.id === openQuestionId}
-                        onClick={() => handleQuestionClick(question.id)}
-                    />
-                ))
-            }
-
-
-
-        </div>
-    )
+  return (
+    <div className="frequentQuestions">
+      <h1 className="title">Frequently asked questions</h1>
+      {generalQuestions.map((question) => (
+        <QuestionItem
+          key={question.id}
+          question={question}
+          isOpen={question.id === openQuestionId}
+          onClick={() => handleQuestionClick(question.id)}
+        />
+      ))}
+    </div>
+  );
 }
--- src/components/services/QuestionItem.jsx
+++ src/components/services/QuestionItem.jsx
@@ -1,26 +1,17 @@
-import { useState } from "react"
-import "../../styles/questioItem.css"
-import Arrow from "../../assets/icons/menu-arrow.icon"
+import "../../styles/questioItem.css";
+import Arrow from "../../assets/icons/menu-arrow.icon";
+import React from "react";
 
-export default function QuestionItem(props) {
-    const { question, isOpen, onClick } = props;
-
-    return (
-        <div onClick={onClick} className={isOpen ? "questionItem open" : "questionItem"}>
-            <div>
-                <h1 className="question">
-                    {question.question}
-                </h1>
-
-                <Arrow className={isOpen ? "icon open" : "icon"} />
-            </div>
-
-            {isOpen && (
-                <span>
-                    {question.answer}
-                </span>
-            )}
-
-        </div>
-    )
+function QuestionItem({ question, isOpen, onClick }) {
+  return (
+    <div onClick={onClick} className={`questionItem ${isOpen ? "open" : ""}`}>
+      <div>
+        <h1 className="question">{question.question}</h1>
+        <Arrow className={`icon ${isOpen ? "open" : ""}`} />
+      </div>
+      {isOpen && <span>{question.answer}</span>}
+    </div>
+  );
 }
+
+export default React.memo(QuestionItem);
