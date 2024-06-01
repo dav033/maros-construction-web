7364da67b5c410d55b768bdc149ae305ed6bca52
david theran morales <david.theran030201@gmail.com>
2024-05-31 20:09:26 -0500

    commit

---
+++ src/assets/icons/menu-arrow.icon.jsx
@@ -0,0 +1,6 @@
+
+export default function Arrow(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="2.5em" height="2.5em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M12 14.379q-.161 0-.298-.053t-.267-.184L7.046 9.754q-.14-.14-.15-.344t.15-.364t.354-.16t.354.16L12 13.292l4.246-4.246q.14-.14.345-.15q.203-.01.363.15t.16.354t-.16.354l-4.389 4.388q-.13.131-.267.184q-.136.053-.298.053"></path></svg>
+  )
+}
