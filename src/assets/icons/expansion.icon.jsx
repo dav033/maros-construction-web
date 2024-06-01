0fcc1ba0bf4420e1481b3377f726df7db9e3ccf3
david theran morales <david.theran030201@gmail.com>
2024-06-01 12:41:42 -0500

    solve styles bugs

---
+++ src/assets/icons/expansion.icon.jsx
@@ -0,0 +1,23 @@
+export default function IconParkOutlineInternalExpansion(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1.5em"
+      height="1.5em"
+      viewBox="0 0 48 48"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      >
+        <path d="M8 42h32a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v32a2 2 0 0 0 2 2"></path>
+        <path d="M42 8a2 2 0 0 0-2-2H28v14h14z" clipRule="evenodd"></path>
+        <path d="m13 35l10-10m0 0v7m0-7h-7"></path>
+      </g>
+    </svg>
+  );
+}
