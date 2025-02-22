5bb00bc0bd5b8b9915af5053a64697cd6546aa9c
david.theran03@gmail.com <david.theran03@gmail.com>
2025-02-22 12:00:43 -0500

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,24 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   output: "server",
   adapter: netlify(),
   adapter: netlify({
-    edgeMiddleware: true,
+    edgeMiddleware: true
   }),
   image: {
-    remotePatterns: [{ protocol: "https" }],
+    remotePatterns: [{
+      protocol: "https"
+    }]
   },
   vite: {
     ssr: {
-      noExternal: ["path-to-regexp"],
-    },
-  },
+      noExternal: ["path-to-regexp"]
+    }
+  }
 });
