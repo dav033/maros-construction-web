ded7f52fb28263f980f7060292f3c2c4699efd30
david.theran03@gmail.com <david.theran03@gmail.com>
2025-02-22 12:29:32 -0500

    commit

--- public/images.js
+++ public/images.js
@@ -21,6 +21,9 @@ import contactUs from "/public/images/contact-us.webp";
 import contactUsMedium from "/public/images/contact-us-medium.webp";
 import contactUsSmall from "/public/images/contact-us-small.webp";
 import originalIcon from "/public/images/maros-icon-original.webp";
+import image1 from "./images/sigmund-uS_MT4wUXf0-unsplash.webp"
+import image2 from "./images/battlecreek-coffee-roasters-NfG4rXmceFM-unsplash.webp"
+import image3 from "./images/ashley-satanosky-mWN686Fsbgs-unsplash.webp"
 
 const images = {
   concrete,
@@ -46,6 +49,9 @@ const images = {
   contactUsMedium,
   contactUsSmall,
   originalIcon,
+  image1,
+  image2,
+  image3
 };
 
 export default images;
