94fcf5dc5e11f3bdd6a8b1ea508c9b7cf487c24b
david.theran03@gmail.com <david.theran03@gmail.com>
2025-02-26 14:12:16 -0500

    commit

--- public/images.js
+++ public/images.js
@@ -21,7 +21,7 @@ import contactUs from "/public/images/contact-us.webp";
 import contactUsMedium from "/public/images/contact-us-medium.webp";
 import contactUsSmall from "/public/images/contact-us-small.webp";
 import originalIcon from "/public/images/maros-icon-original.webp";
-import image1 from "./images/sigmund-uS_MT4wUXf0-unsplash.webp"
+import image1 from "./images/sigmund-4CNNH2KEjhc-unsplash.webp"
 import image2 from "./images/battlecreek-coffee-roasters-NfG4rXmceFM-unsplash.webp"
 import image3 from "./images/ashley-satanosky-mWN686Fsbgs-unsplash.webp"
 
